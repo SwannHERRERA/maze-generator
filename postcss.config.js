71e926f76c8135272f469d86cc3fdd4f7544585f
Swann <swann@devloup.dev>
2020-12-29 12:09:13 +0100

    postcss-config ok

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,16 @@
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./public/index.html"],
-  css: ["./assets/css/*.css"],
-
-  // Include any special characters you're using in this regular expression
-
-  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-});
-
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    purgecss: process.env.NODE_ENV === "production" ? purgecss : "",
-  },
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    process.env.NODE_ENV === "production"
+      ? require("@fullhuman/postcss-purgecss")({
+          content: ["./public/index.html"],
+          css: ["./assets/css/*.css"],
+
+          // Include any special characters you're using in this regular expression
+
+          defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+        })
+      : "",
+  ],
 };
