f1ae91f954fa64056b23bb33cc491deb9752773b
Coraline Esedji <cesedji@myges.fr>
2021-01-15 11:19:29 +0100

    changes end-to-end test

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -4,8 +4,20 @@ context("Actions", () => {
   beforeEach(() => {
     cy.visit("localhost:8080");
   });
+
+
   it("sould contain basic of the app", () => {
-    cy.get("h1").should("contain", "Maze generator");
-    cy.get("form");
+    cy.get('[data-cy=title]').should("contain", "Maze generator");
+    cy.get('[data-cy=submit-form]');
+    cy.get('[data-cy=submit-button]');
+    cy.get('[data-cy=form-select-maze]');
+    cy.get('[data-cy=form-input-start]');
+    cy.get('[data-cy=form-input-color]');
+    cy.get('[data-cy=form-select-sale]');
   });
+
+  it("should generate the maze", () => {
+      cy.get('[data-cy=submit-form]').click();
+
+    });
 });
