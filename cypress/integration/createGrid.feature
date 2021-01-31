406796d909c7d928d637ec46d3c93696ee0b88dd
Swann <swann@devloup.dev>
2021-01-31 11:37:08 +0100

    create unit test in cypress

--- cypress/integration/createGrid.feature
+++ cypress/integration/createGrid.feature
@@ -1,7 +1,7 @@
 # features/bank-account.feature
 Feature: create Grid
 
-  Scenario: test init grid function
+  Scenario Outline: test init grid function
     Given I want to create a grid of isolate cell of size <size>
     When i run createGrid
     Then It should return a array with isolate cell of 0 by -1 of size <size>
@@ -15,5 +15,4 @@ Feature: create Grid
 
   Scenario: test very small grid
     Given a size of 2
-    When i want to create maze
-    Then it should throw an error
+    When i want to create a maze it should throw an error
--- cypress/integration/createGrid/createGrid.ts
+++ cypress/integration/createGrid/createGrid.ts
@@ -1,43 +1,33 @@
 import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
+import Maze from "../../../src/Maze";
+import { assert, expect } from "chai";
+import Random from "../../../src/Random";
+import getDefaultMazeConfig from "../../../src/step-definitions/MazeTestHelper";
+import { MazeConfig } from "../../../src/MazeConfig";
+import FakeRand from "../../../src/FakeRand";
 
-@binding()
-export class CreateGridSteps {
-  private size: number = 0;
-  private config: MazeConfig = getDefaultMazeConfig();
-  private random: Random = new FakeRand();
+const size: number = 0;
+const config: MazeConfig = getDefaultMazeConfig();
+const random: Random = new FakeRand();
 
-  private maze: Maze;
+let maze: Maze;
 
-  constructor() {
-    this.maze = new Maze(this.config, this.random);
-  }
+Given("I want to create a grid of isolate cell of size {number}", (size) => {
+  config.size = size;
+  maze = new Maze(config, random);
+});
 
-  @given(/I want to create a grid of isolate cell of size (\d*)/)
-  createAGridOfIsolateCell(size: string) {
-    this.config.size = Number(size);
+When("i run createGrid", () => {
+  maze.createGrid();
+});
 
-    this.maze = new Maze(this.config, this.random);
-  }
+When("i want to create a maze it should throw an error", () => {
+  expect(() => new Maze(config, random)).to.throw();
+});
 
-  @given(/a size of (\d*)/)
-  givenASizeOf(size: string) {
-    this.config.size = Number(size);
-  }
-
-  @when(/i run createGrid/)
-  whenIRunCreateGrid() {
-    this.maze.createGrid();
-  }
-
-  @when(/i want to create maze/)
-  @then(/it should throw an error/)
-  whenIWantToCreateMaze() {
-    expect(() => new Maze(this.config, this.random)).to.throw();
-  }
-
-  @then(/It should return a array with isolate cell of 0 by -1 of size (\w+)/)
-  itSouldBeAnArrayOfIsolateCell(sizeStr: string) {
-    const size = Number(sizeStr);
+Then(
+  "It should return a array with isolate cell of 0 by -1 of size {number}",
+  () => {
     const expected = new Map();
     expected.set(3, [
       [-1, -1, -1],
@@ -66,8 +56,8 @@ export class CreateGridSteps {
       [-1, 0, -1, 0, -1, 0, -1],
       [-1, -1, -1, -1, -1, -1, -1],
     ]);
-    const actual = this.maze.getMaze();
+    const actual = maze.getMaze();
 
     assert.deepEqual(actual, expected.get(size));
   }
-}
+);
